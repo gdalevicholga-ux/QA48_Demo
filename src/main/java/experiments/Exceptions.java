a6dbcfde495e033ae2a7504614b10edcaf5c11a1
gdalevicholga-ux <gdalevicholga@gmail.com>
2025-08-16 19:33:49 +0300

    Exception1

---
+++ src/main/java/experiments/Exceptions.java
@@ -0,0 +1,37 @@
+package experiments;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        String[] strArray = {"str1", "str2", "str3"};
+        uncheckedExceptionStr(strArray);
+        try {
+            pause();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void uncheckedExceptionStr(String[] str) {
+           try {
+               for (int i = 0; i < 5; i++) {
+                   System.out.println(str[i]);
+               }
+           }catch (ArrayIndexOutOfBoundsException e){
+               e.printStackTrace();
+               System.out.println("Created exception!");
+           }
+        System.out.println("Program is working!");
+    }
+    private static void pause() throws InterruptedException {
+        checkkedException();
+        System.out.println("pause 3 sec");
+    }
+    private static void checkkedException() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    private String first(String s) {
+        return null;
+    }
+}
